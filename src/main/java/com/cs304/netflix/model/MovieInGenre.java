d60e8297689990aff246b62bd5a0156df13532b5
patye <patricia.ye@smus.ca>
2018-06-15 11:46:49 -0700

    Movie and Watch count model added

--- src/main/java/com/cs304/netflix/model/MovieInGenre.java
+++ src/main/java/com/cs304/netflix/model/MovieInGenre.java
@@ -1,14 +1,22 @@
 package com.cs304.netflix.model;
 
+import java.math.BigDecimal;
+
 public class MovieInGenre {
-    String movieTitle;
-    String genreName;
+   BigDecimal id, duration, releaseYear;
+    String genreName, movieTitle, ageRestriction;
 
     public MovieInGenre(){}
 
-    public MovieInGenre(String movieTitle, String genreName){
+    public MovieInGenre(String movieTitle, String genreName, String ageRestriction,
+                        BigDecimal id, BigDecimal duration, BigDecimal releaseYear){
         this.movieTitle = movieTitle;
         this.genreName = genreName;
+        this.ageRestriction = ageRestriction;
+        this.id = id;
+        this.duration = duration;
+        this.releaseYear = releaseYear;
+
     }
 
     public String getMovieTitle(){
@@ -26,4 +34,37 @@ public class MovieInGenre {
     public void setGenreName(String genreName) {
         this.genreName = genreName;
     }
+
+    public BigDecimal getDuration() {
+        return duration;
+    }
+
+    public BigDecimal getId() {
+        return id;
+    }
+
+    public BigDecimal getReleaseYear() {
+        return releaseYear;
+    }
+
+    public String getAgeRestriction() {
+        return ageRestriction;
+    }
+
+    public void setAgeRestriction(String ageRestriction) {
+        this.ageRestriction = ageRestriction;
+    }
+
+    public void setDuration(BigDecimal duration) {
+        this.duration = duration;
+    }
+
+    public void setId(BigDecimal id) {
+        this.id = id;
+    }
+
+    public void setReleaseYear(BigDecimal releaseYear) {
+        this.releaseYear = releaseYear;
+    }
 }
+
