9c953dc0396945e56f22103ee4d51092c69f6ef8
Michelle Huh <michelle.huh@hotmail.com>
2018-06-16 17:13:55 -0700

    Profile update, create controller and mapper added

---
+++ src/main/java/com/cs304/netflix/controller/ProfileInfoController.java
@@ -0,0 +1,38 @@
+package com.cs304.netflix.controller;
+
+import com.cs304.netflix.mapper.ProfileInfoMapper;
+import com.cs304.netflix.model.Profile;
+import com.cs304.netflix.model.Response;
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@MapperScan("com.cs304.netflix.mapper")
+public class ProfileInfoController {
+
+    public static final Logger logger = LoggerFactory.getLogger(ProfileInfoController.class);
+
+    @Autowired(required=false)
+    ProfileInfoMapper mapper;
+
+    @PostMapping("/admin/profile/create")
+    public ResponseEntity<Response> login(@RequestBody Profile profile) {
+
+        Profile result;
+        try {
+            mapper.createProfile(profile);
+            result = mapper.getProfileByAdminIdAndName(profile);
+            return new ResponseEntity<Response>(new Response(result), HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error(e.getLocalizedMessage());
+            return new ResponseEntity<Response>(new Response(e), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ src/main/java/com/cs304/netflix/mapper/ProfileInfoMapper.java
@@ -0,0 +1,38 @@
+package com.cs304.netflix.mapper;
+
+import com.cs304.netflix.model.Profile;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+@Mapper
+public interface ProfileInfoMapper {
+
+    @Select("SELECT * \n" +
+            "  FROM Profile \n" +
+            " WHERE adminId = #{adminId}\n" +
+            "   and id = #{id}")
+    Profile getProfileByAdminIdAndId(Profile profile);
+
+    @Select("SELECT * \n" +
+            "  FROM Profile \n" +
+            " WHERE adminId = #{adminId}\n" +
+            "   and name = #{name}")
+    Profile getProfileByAdminIdAndName(Profile profile);
+
+    @Update("UPDATE Profile \n" +
+            "   SET age = #{age} \n" +
+            " WHERE adminId = #{adminId} \n" +
+            "   AND id = #{id}")
+    Profile updateAge(Profile profile);
+
+    @Insert("INSERT INTO Profile ( id, name, adminId, age )\n" +
+            "VALUES (((SELECT max(id) \n" +
+            "             FROM Profile \n" +
+            "             WHERE adminId = #{adminId}) + 1)\n" +
+            "         , #{name}\n" +
+            "         , #{adminId}\n" +
+            "         , #{age})")
+    boolean createProfile(Profile profile);
+}
